84d089a3ba7bdb40dcf46010c8500443f91f5d9c
Yuriy Guts <yuriy.guts@gmail.com>
2017-10-21 22:21:43 +0300

    Initial package structure

---
+++ README.rst
@@ -0,0 +1,4 @@
+libcheckersgame
+---------------
+
+International checkers gameplay library for the CS301 AI course at UCU.
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='libcheckersgame',
+    version='0.1.1',
+    description='International checkers gameplay library for the CS301 AI course at UCU',
+    url='https://github.com/YuriyGuts/libcheckersgame',
+    author='Yuriy Guts',
+    author_email='yuriy.guts@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        'pytest',
+    ],
+)
